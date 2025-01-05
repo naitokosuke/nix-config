ab422d9ab00417ba939b1b1effd08069a53070fa
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-01-05 22:53:53 +0900

    init

---
+++ home-manager/git.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "engineer-naito";
+    userEmail = "kosuke.naito.engineer@gmail.com";
+    extraConfig = {
+      core.editor = "vim";
+      init.defaultBranch = "main";
+    };
+  };
+}
