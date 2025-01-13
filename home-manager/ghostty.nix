1937e51e770bdbebb37e001a60155c2cef8aced8
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-01-13 12:06:23 +0900

    add ghostty config

---
+++ home-manager/ghostty.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, lib, ... }:
+
+let
+  ghosttyConfig = ''
+    theme = "catppuccin-mocha"
+  '';
+in
+{
+  # Ghosttyの設定ファイルを配置
+  home.file.".config/ghostty/config".text = ghosttyConfig;
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./ghostty.nix
     ./git.nix
     ./zsh.nix
   ];
