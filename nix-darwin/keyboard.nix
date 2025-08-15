63293a159fc35d9293a83a28ac557905b4a83bc1
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-08-15 13:50:46 +0900

    feat: disable Ctrl+Space input source switching shortcuts

---
+++ nix-darwin/keyboard.nix
@@ -0,0 +1,16 @@
+{
+  system.defaults.CustomUserPreferences = {
+    "com.apple.symbolichotkeys" = {
+      AppleSymbolicHotKeys = {
+        # Disable "Select the previous input source" (Ctrl+Space)
+        "60" = {
+          enabled = false;
+        };
+        # Disable "Select the next source in the Input Menu" (Ctrl+Option+Space)
+        "61" = {
+          enabled = false;
+        };
+      };
+    };
+  };
+}
