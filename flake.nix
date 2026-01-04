52f8b7b7573234636bb03b991208580ec0c5d18e
ナイトウコウスケ <102337893+naitokosuke@users.noreply.github.com>
2026-01-04 09:12:25 +0900

    126 add mac heavy config (#127)
    * feat: enhance Mac-heavy configuration with additional system packages and settings
    
    * c
    
    * cspell

--- flake.nix
+++ flake.nix
@@ -42,74 +42,83 @@
         inherit system;
         config.allowUnfree = true;
       };
+
+      # Common Darwin system configuration
+      mkDarwinConfig =
+        { hostName }:
+        nix-darwin.lib.darwinSystem {
+          modules = (
+            [
+              {
+                networking.hostName = hostName;
+                networking.computerName = hostName;
+
+                system.primaryUser = "naitokosuke";
+
+                nixpkgs.config.allowUnfree = true;
+                nixpkgs.hostPlatform = system;
+
+                programs.zsh.enable = true;
+
+                environment.systemPackages = with pkgs; [
+                  devbox
+                  fcp
+                  fd
+                  fzf
+                  gh
+                  ghq
+                  git
+                  mise
+                  pnpm
+                  ripgrep
+                  tree
+                  uv
+                  vim
+                ];
+
+                nix.settings.experimental-features = "nix-command flakes";
+
+                system.configurationRevision = self.rev or self.dirtyRev or null;
+
+                system.stateVersion = 5;
+              }
+              home-manager.darwinModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = "backup";
+                home-manager.users.naitokosuke = import ./home-manager/home.nix;
+                home-manager.extraSpecialArgs = {
+                  inherit vscode-settings;
+                };
+              }
+              nix-homebrew.darwinModules.nix-homebrew
+              {
+                nix-homebrew = {
+                  enable = true;
+                  # Install Homebrew under the default Intel prefix for Rosetta 2
+                  enableRosetta = true;
+                  user = "naitokosuke";
+                  # Automatically migrate existing Homebrew installation
+                  autoMigrate = true;
+                  taps = {
+                    "homebrew/homebrew-cask" = homebrew-cask;
+                  };
+                  # Declarative tap management
+                  mutableTaps = false;
+                };
+              }
+            ]
+            ++ (import ./nix-darwin)
+          );
+        };
     in
     {
-      darwinConfigurations."Mac-big" = nix-darwin.lib.darwinSystem {
-        modules = (
-          [
-            {
-              networking.hostName = "Mac-big";
-              networking.computerName = "Mac-big";
+      # Mac mini
+      darwinConfigurations."Mac-big" = mkDarwinConfig { hostName = "Mac-big"; };
 
-              system.primaryUser = "naitokosuke";
-
-              nixpkgs.config.allowUnfree = true;
-              nixpkgs.hostPlatform = system;
-
-              programs.zsh.enable = true;
-
-              environment.systemPackages = with pkgs; [
-                devbox
-                fcp
-                fd
-                fzf
-                gh
-                ghq
-                git
-                mise
-                pnpm
-                ripgrep
-                tree
-                uv
-                vim
-              ];
-
-              nix.settings.experimental-features = "nix-command flakes";
-
-              system.configurationRevision = self.rev or self.dirtyRev or null;
-
-              system.stateVersion = 5;
-            }
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.users.naitokosuke = import ./home-manager/home.nix;
-              home-manager.extraSpecialArgs = {
-                inherit vscode-settings;
-              };
-            }
-            nix-homebrew.darwinModules.nix-homebrew
-            {
-              nix-homebrew = {
-                enable = true;
-                # Install Homebrew under the default Intel prefix for Rosetta 2
-                enableRosetta = true;
-                user = "naitokosuke";
-                # Automatically migrate existing Homebrew installation
-                autoMigrate = true;
-                taps = {
-                  "homebrew/homebrew-cask" = homebrew-cask;
-                };
-                # Declarative tap management
-                mutableTaps = false;
-              };
-            }
-          ]
-          ++ (import ./nix-darwin)
-        );
-      };
+      # MacBook Air
+      darwinConfigurations."Macbook-heavy" = mkDarwinConfig { hostName = "Macbook-heavy"; };
 
       # Use treefmt for formatting with nixfmt
       formatter.${system} = treefmt-nix.lib.mkWrapper pkgs {
