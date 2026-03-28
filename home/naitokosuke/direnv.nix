d700616f06feda9ae135486e798cac62ce098c86
ナイトウコウスケ <102337893+naitokosuke@users.noreply.github.com>
2026-03-28 11:55:45 +0900

    fix: workaround direnv CGO_ENABLED build  failure on aarch64-darwin (#283)
    * fix: workaround direnv CGO_ENABLED build  failure on aarch64-darwin
    
    * chore

--- home/naitokosuke/direnv.nix
+++ home/naitokosuke/direnv.nix
@@ -1,8 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.direnv = {
     enable = true;
+    # FIXME: Remove this override once the fix lands in nixpkgs-unstable
+    # https://github.com/naitokosuke/dotfiles/pull/283
+    # https://github.com/NixOS/nixpkgs/issues/504092
+    # https://github.com/NixOS/nixpkgs/pull/502769
+    package = pkgs.direnv.overrideAttrs (old: {
+      env = (old.env or { }) // {
+        CGO_ENABLED = 1;
+      };
+    });
     nix-direnv.enable = true;
     enableNushellIntegration = true;
   };
