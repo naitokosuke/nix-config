693d36f67c30be2333fba34073cdeefe8c3b772a
naitokosuke <kosuke.naito.engineer@gmail.com>
2026-03-28 11:55:21 +0900

    chore

--- home/naitokosuke/direnv.nix
+++ home/naitokosuke/direnv.nix
@@ -4,6 +4,7 @@
   programs.direnv = {
     enable = true;
     # FIXME: Remove this override once the fix lands in nixpkgs-unstable
+    # https://github.com/naitokosuke/dotfiles/pull/283
     # https://github.com/NixOS/nixpkgs/issues/504092
     # https://github.com/NixOS/nixpkgs/pull/502769
     package = pkgs.direnv.overrideAttrs (old: {
